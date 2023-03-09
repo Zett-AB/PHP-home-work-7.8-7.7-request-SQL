35099a87cc4df43fb92ac9b71a789a115c30a281
Alexander <formysite.ru@yandex.ru>
2023-03-09 16:57:32 +0300

    begin

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+</head>
+
+<body>
+    <main>
+        <header>
+            <h1>
+                Домашняя работа по SQL запросам JOIN 
+            </h1>
+            <h4 class="subtitle">
+                Работа по урокам 7.7. и 7.8 
+            </h4>
+        </header>
+        <section class="code-php">
+            <?php
+                $nickname="";
+                $hello="";
+                $offer="";
+
+                echo 
+            ?>
+        </section>
+        <section class="begin">
+            <div class="bg-task">
+
+                <h5>
+                    Задание 1
+                </h5>
+                <p>
+
+                </p>
+                <p></p>
+                <p></p>
+            </div>
+            <div class=""></div>
+            <div class=""></div>
+        </section>
+        <section class=""></section>
+        <section class=""></section>
+        <section class=""></section>
+    </main>
+</body>
+
+</html>
